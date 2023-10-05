6f54e674ec7fdd6db0659dcd42238f2812a6adb1
asya474 <51288093+asya474@users.noreply.github.com>
2023-10-05 15:11:12 +0600

    Update homework.py

--- homework.py
+++ homework.py
@@ -3,7 +3,6 @@ from datetime import time
 
 def test_dark_theme_by_time():
     current_time = time(hour=23)
-    is_dark_theme = None
     if time(hour=22) <= current_time or current_time < time(hour=6):
         is_dark_theme = True
     else:
@@ -15,7 +14,6 @@ def test_dark_theme_by_time_and_user_choice():
 
     current_time = time(hour=16)
     dark_theme_enabled_by_user = True
-    is_dark_theme = None
     if dark_theme_enabled_by_user is not None:
         is_dark_theme = dark_theme_enabled_by_user
     else:
@@ -36,7 +34,7 @@ def test_find_suitable_user():
         {"name": "Maria", "age": 18},
     ]
 
-    suitable_users = [i for i in users if ["name"] == "Olga"]
+    suitable_users = [i for i in users if i["name"] == "Olga"]
     suitable_users = suitable_users[0]
     assert suitable_users == {"name": "Olga", "age": 45}
 
@@ -67,3 +65,8 @@ def open_browser(browser_name):
 def go_to_companyname_homepage(page_url):
     actual_result = print_function_info(go_to_companyname_homepage, page_url=page_url)
     assert actual_result == "Go To Companyname Homepage [https://companyname.com]"
+
+
+def find_registration_button_on_login_page(page_url, button_text):
+    actual_result = print_function_info(find_registration_button_on_login_page, page_url=page_url, button_text=button_text)
+    assert actual_result == "Find Registration Button On Login Page [https://companyname.com/login, Register]"
